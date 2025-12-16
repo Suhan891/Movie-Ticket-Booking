a57f152b7bda04190ae24f3d48110c2b626ed68e
Suhan Patra <patra.suhan2006@gmail.com>
2025-12-16 20:21:05 +0530

    File Setup on Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,31 @@
-import { useState } from 'react'
+
+import {Routes, Route, useLocation} from 'react-router-dom'
+
+import Navbar from './components/Navbar'
+import Home from './pages/Home'
+import Movie from './pages/Movie'
+import MovieDetails from './pages/MovieDetails'
+import MyBooking from './pages/MyBooking'
+import SeatLayout from './pages/SeatLayout'
+import Favourite from './pages/Favourite'
+import Footer from './components/Footer'
 
 function App() {
   
-
+  const isAdmin = useLocation().pathname.startsWith('/admin');
   return (
-    <></>
+    <>
+      {!isAdmin && <Navbar />}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movie />} />
+        <Route path="/movie/:id" element={<MovieDetails />} />
+        <Route path="/my-booking" element={<MyBooking />} />
+        <Route path="/movie/:id/:date" element={<SeatLayout />} />
+        <Route path="/favourite" element={<Favourite/>} />
+      </Routes>
+      {!isAdmin && <Footer />}
+    </>
   )
 }
 
