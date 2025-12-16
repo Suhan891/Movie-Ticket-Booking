f6dad47c9c7fecf53013579649f8f4a5ccc6ebdc
Suhan Patra <patra.suhan2006@gmail.com>
2025-12-17 00:56:26 +0530

    Made Half of the Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import FeaturedSection from '../components/FeaturedSection'
 
 const Home = () => {
   return (
     <div>
+      <Hero />
+      <FeaturedSection />
       Home
     </div>
   )
