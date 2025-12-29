abb12d2e166e0815790306973704c29f8635af3a
Suhan Patra <patra.suhan2006@gmail.com>
2025-12-29 18:18:50 +0530

    Rate Limit on Forgot Password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const { registerUser, loginUser, verifyEmail, refreshToken, logout, forgotPassword, resetPasswordHandler,  googleAuthStartHandler, googleAuthCallbackHandler } = require("../controlllers/user")
 const requireAuth = require("../middlewares/requireAuth")
+const limiter = require("../middlewares/rateLimiter")
 
 const router = express.Router()
 
@@ -9,7 +10,7 @@ router.post("/login",loginUser)
 router.get("/email-verify",verifyEmail)
 router.post("/refreshToken",refreshToken)
 router.post("/logout",logout)
-router.post("/forgot-password",forgotPassword)
+router.post("/forgot-password",limiter,forgotPassword)
 router.post("/reset-password",resetPasswordHandler)
 router.get("/google",googleAuthStartHandler)
 router.get("/google/callback",googleAuthCallbackHandler)
