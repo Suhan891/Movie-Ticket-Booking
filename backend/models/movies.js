db88357434b37e68726b6bb43a343169e0c561c6
Suhan Patra <patra.suhan2006@gmail.com>
2026-01-08 00:14:31 +0530

    Movie Schema

---
+++ backend/models/movies.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongooose")
+
+const movieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    cast: {
+        type: [String],
+        required: true
+    },
+    trailerUrl: {
+        type: String,
+        required: true
+    },
+    language: {
+        type: String,
+        default: "English"
+    },
+    director:{
+        type: String,
+        required: true
+    },
+    releaseDate:{
+        type: String,
+        required: true
+    },
+    releasedStatus:{
+        type: String,
+        default: "Released"
+    }
+},{timestamps: true})
+
+const Movies = mongoose.model("Movies",movieSchema)
+module.exports = Movies
